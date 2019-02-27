0b02c737a49a2a00a5cd7f8ce8abec4f9cf95f51
Xiao Zhang <griffcho16@gmail.com>
2019-02-26 20:41:26 -0600

    new commit

--- Lab 1/README.txt
+++
@@ -1,2 +0,0 @@
-student 1: Zakaria Kofiro, 9074188286, kofiro@wisc.edu
-student 2: Griff Zhang, 9078214609, xzhang953@wisc.edu
---
+++ Lab1/myswitch.py
@@ -0,0 +1,37 @@
+'''
+Ethernet learning switch in Python.
+
+Note that this file currently has the code to implement a "hub"
+in it, not a learning switch.  (I.e., it's currently a switch
+that doesn't learn.)
+'''
+from switchyard.lib.userlib import *
+
+def main(net):
+    my_interfaces = net.interfaces() 
+    mymacs = [intf.ethaddr for intf in my_interfaces]
+    cache = {}
+
+    while True:
+        try:
+            timestamp,input_port,packet = net.recv_packet()
+        except NoPackets:
+            continue
+        except Shutdown:
+            return
+
+        log_debug ("In {} received packet {} on {}".format(net.name, packet, input_port))
+        if packet[0].dst in mymacs:
+            log_debug ("Packet intended for me")
+        else:
+            if packet[0].dst not in cache:
+                cache[packet[0].dst] = cache.pop(packet[0].dst)
+                for intf in my_interfaces:
+                    if input_port != intf.name:
+                        net.send_packet(intf.name, packet)
+            else:  # send the
+                for intf in my_interfaces:
+                    if input_port != intf.name:
+                        log_debug ("Flooding packet {} to {}".format(packet, intf.name))
+                        net.send_packet(intf.name, packet)
+    net.shutdown()
