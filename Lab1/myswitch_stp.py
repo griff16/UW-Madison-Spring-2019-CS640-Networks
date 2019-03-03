98a8cab3aff8ab70aac9e9ef99f48afe4a5c7cb3
Xiao Zhang <griffcho16@gmail.com>
2019-03-03 14:28:48 -0600

    update

--- Lab1/myswitch_stp.py
+++ Lab1/myswitch_stp.py
@@ -1,38 +1,32 @@
 from switchyard.lib.userlib import *
 from spanningtreemessage import *
 
-class MySwitch_stp(root="00:00:00:00:00:00", hops_to_root = 0):
-    curRoot = "FF:FF:FF:FF:FF:FF"
-    hopsToRoot = 0
-
+class MySwitch_stp:
     def __init__(self, root, hops_to_root = 0):
         self.curRoot = root
         self.hopsToRoot = hops_to_root
 
-    def minMac (self, mymacs):  # return the lowest mac addr in the switch
-        minMac = "FF:FF:FF:FF:FF:FF"
-        for min in mymacs:
-            if minMac < min:
-                minMac = min
-        return minMac
+
+
+
+def minMac (mymacs):  # return the lowest mac addr in the switch
+    minMac = "FF:FF:FF:FF:FF:FF"
+    for min in mymacs:
+        if minMac < min:
+            minMac = min
+    return minMac
 
 def main (net):
     my_interfaces = net.interfaces()
     mymacs = [intf.ethaddr for intf in my_interfaces]
 
-    # minMac(mymacs)
-    for min in mymacs:
-        if min < curRoot:
-            curRoot = min
+    min = minMac(mymacs)
 
     # creating the header
-    spm = SpanningTreeMessage(curRoot)
+    spm = SpanningTreeMessage(min)
     Ethernet.add_next_header_class(EtherType.SLOW, SpanningTreeMessage)
     pkt = Ethernet(src="11:22:11:22:11:22", dst="22:33:22:33:22:33", ethertype=EtherType.SLOW) + spm
 
     # flood the STP
     for intf in my_interfaces:  # flood the packet
         net.send_packet(intf.name, packet)
-
-if __name__== "__main__":
-    main(net)
